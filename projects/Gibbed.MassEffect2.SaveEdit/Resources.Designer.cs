a935d66bfe3dbb5decc5a33ba6631beb65474904
Rick Gibbed <rick@gibbed.us>
2021-05-28 03:28:10 -0500

    Fix resources base name.

--- projects/Gibbed.MassEffect2.SaveEdit/Resources.Designer.cs
+++ projects/Gibbed.MassEffect2.SaveEdit/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace Gibbed.MassEffect2.SaveEdit {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Gibbed.MassEffect2.SaveEdit.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Gibbed.MassEffect2.SaveEdit.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
